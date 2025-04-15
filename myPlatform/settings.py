33fd76b0ce15fbe0915df22eb414a99300998c0a
Yevheniy <Genya421@gmail.com>
2025-04-15 22:44:14 +0300

    allowe host

--- myPlatform/settings.py
+++ myPlatform/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51-20-31-87.nip.io', '51.20.31.87', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.21.53.216.nip.io', '51.21.53.216', '127.0.0.1', 'localhost']
 
 
 # Application definition
