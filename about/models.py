e027c99ac92215b76babaf68ea386f55956a529e
Yevheniy <Genya421@gmail.com>
2025-06-26 13:54:07 +0300

    add admin panel for about

--- about/models.py
+++ about/models.py
@@ -1,70 +1,71 @@
 from django.db import models
 
 # Create your models here.
-
-class Address(models.Model):
-    city = models.CharField()
-    country = models.CharField()
-
-class Links(models.Model):
-    name = models.CharField()
-    url = models.URLField()
-
 class AboutMe(models.Model):
     first_name = models.CharField()
     second_name = models.CharField()
     descriptions = models.TextField()
     short_description = models.CharField(blank=True, null=True)
     email = models.EmailField()
-    address = models.ForeignKey(to=Address, on_delete=models.CASCADE)
-    links = models.ForeignKey(to=Links, on_delete=models.CASCADE)
 
+class Address(models.Model):
+    city = models.CharField()
+    country = models.CharField()
+    about = models.OneToOneField(to=AboutMe, on_delete=models.CASCADE)
 
-class Description(models.Model):
-    text = models.TextField()
-
-class Tags(models.Model):
-    name = models.CharField()
-    descriptions = models.ForeignKey(to=Description, on_delete=models.CASCADE)
 
 class Projects(models.Model):
     name = models.CharField()
     descriptions = models.TextField()
     instruments = models.TextField(blank=True, null=True)
-    tags = models.ForeignKey(to=Tags, on_delete=models.CASCADE, blank=True, null=True)
-    links = models.ForeignKey(to=Links, on_delete=models.CASCADE, blank=True, null=True)
+    
+class Tag(models.Model):
+    name = models.CharField()
+    project = models.ForeignKey(to=Projects, on_delete=models.CASCADE)
 
+class Description(models.Model):
+    text = models.TextField()
+    tag = models.ForeignKey(to=Tag, on_delete=models.CASCADE)
+
+
+class Links(models.Model):
+    name = models.CharField()
+    url = models.URLField()
+    about = models.ForeignKey(to=AboutMe, on_delete=models.CASCADE, blank=True, null=True)
+    project = models.ForeignKey(to=Projects, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Education(models.Model):
+    descriptions = models.TextField()
 
 class Course(models.Model):
     name = models.CharField()
     descriptions = models.TextField()
+    education = models.ForeignKey(to=Education, on_delete=models.CASCADE)
 
 class Lection(models.Model):
     name = models.CharField()
     descriptions = models.TextField()
+    education = models.ForeignKey(to=Education, on_delete=models.CASCADE)
 
 class Book(models.Model):
     name = models.TextField()
     author = models.CharField()
+    education = models.ForeignKey(to=Education, on_delete=models.CASCADE)
     
-class Education(models.Model):
-    descriptions = models.TextField()
-    courses = models.ForeignKey(to=Course, on_delete=models.CASCADE, blank=True, null=True)
-    lections = models.ForeignKey(to=Lection, on_delete=models.CASCADE, blank=True, null=True)
-    books = models.ForeignKey(to=Book, on_delete=models.CASCADE, blank=True, null=True)
 
+class Skills(models.Model):
+    descriptions = models.TextField()
 
 class WorkFlow(models.Model):
     name = models.TextField()
+    skills = models.ForeignKey(to=Skills, on_delete=models.CASCADE, blank=True, null=True)
 
 class Instrument(models.Model):
     name = models.CharField()
+    skills = models.ForeignKey(to=Skills, on_delete=models.CASCADE, blank=True, null=True)
 
-class Skills(models.Model):
-    workflows = models.ForeignKey(to=WorkFlow, on_delete=models.CASCADE, blank=True, null=True)
-    instruments = models.ForeignKey(to=Instrument, on_delete=models.CASCADE, blank=True, null=True)
-
-
+    
 class Hobbies(models.Model):
     descriptions = models.TextField()
 
